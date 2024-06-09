4dc79d39d61b8dd6a14764fdc8154421b151cffd
jessLijie <wong.lijie1105@gmail.com>
2024-06-09 12:10:49 +0800

    add tables (comment+talk)

--- api/createdb.php
+++ api/createdb.php
@@ -1,15 +1,15 @@
 <?php
-include("./config.php");
+include ("./config.php");
 //  $db = new db();
 //     $con = $db->connect();
 
 $con = mysqli_connect("localhost", "root", "jkty12138", "unitalk");
-if(!$con){
-    die('Could not connect: '.mysqli_connect_error());
+if (!$con) {
+    die('Could not connect: ' . mysqli_connect_error());
 }
 
 
-$sql1 = "CREATE TABLE users (
+$createUser = "CREATE TABLE users (
     id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     username varchar(50) NOT NULL,
     email varchar(255) NOT NULL,
@@ -17,9 +17,29 @@ $sql1 = "CREATE TABLE users (
     role varchar(20) NOT NULL,
     CONSTRAINT UC_User_Username UNIQUE (username),
     CONSTRAINT UC_User_Email UNIQUE (email)
+    );";
+mysqli_query($con, $createUser);
 
-)";
-mysqli_query($con, $sql1);
+$createTalk = "CREATE TABLE talks (
+     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    img VARCHAR(255),
+    posted_datetime DATETIME NOT NULL,
+    likes int DEFAULT 0
+    );";
+mysqli_query($con, $createTalk);
+
+$createComment = "CREATE TABLE comments(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    comment_content TEXT NOT NULL,
+    user_id INT NOT NULL,
+    talk_id INT NOT NULL,
+    parent_id INT NOT NULL,
+    posted_datetime DATETIME NOT NULL
+    );";
+mysqli_query($con, $createComment);
 
 
 $sql = "INSERT INTO users (id, username, email, password, role) VALUES
@@ -31,6 +51,8 @@ $sql = "INSERT INTO users (id, username, email, password, role) VALUES
 ";
 
 mysqli_query($con, $sql);
+
+
 
 echo "Tables created";
 mysqli_close($con);
